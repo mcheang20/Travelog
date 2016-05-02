2227e4746825edadf5feebfed2892b6051ef3edd
mattcheang <cheangmatt@gmail.com>
2016-05-02 16:54:17 -0700

    user roles change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,22 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :role
+
+  user_roles = {
+    :standard => 0,
+    :premium => 10,
+    :admin => 20
+  }
   has_many :wikis, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+
+  after_initialize :user_default
+
+  def user_default
+    self.role ||= :standard
+  end
 end
