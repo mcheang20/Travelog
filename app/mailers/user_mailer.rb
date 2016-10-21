8d2b12e6ee16d9a40fffb454d94950a17a713f1a
mattcheang <cheangmatt@gmail.com>
2016-10-20 17:57:11 -0700

    add style change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,8 @@
 class UserMailer < ApplicationMailer
-  default from: "mattcheang@yahoo.com"
+  default from: 'mattcheang@yahoo.com'
 
   def new_user(user)
     @user = user
-    headers["Message-ID"] = "<#{@user}@your-app-name.example>"
-
-    mail(to: user.email, subject: "Welcome to Blocipedia")
+    mail(to: @user.email, subject: "Welcome to Blocipedia")
    end
 end
