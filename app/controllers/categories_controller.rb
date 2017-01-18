b4b6dec52f261f06d9c5a59120ad28ed441d298f
mattcheang <cheangmatt@gmail.com>
2017-01-17 20:24:06 -0800

    update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,5 @@
 class CategoriesController < ApplicationController
 
-  def index
-    @categories = Category.all
-  end
-
   def show
     @categories = Category.all
     @category = Category.where(id: params[:id])
