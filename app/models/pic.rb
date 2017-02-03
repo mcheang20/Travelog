92ed43bd493dd3e15e07dad5403227641bbc6628
mattcheang <cheangmatt@gmail.com>
2017-02-02 16:03:14 -0800

    fix pictures view

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,5 +1,5 @@
 class Pic < ActiveRecord::Base
   belongs_to :wiki
-  has_attached_file :image, styles: { medium: "400x400", thumb: "100x100", album: "600x600", big: "800x800"}, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "500x500", thumb: "100x100", album: "600x600", big: "1000x1000"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
